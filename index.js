db845c121cc5206c49a24c7fa1b65ac75f4bd9a9
Deansie <dennis.andersen@lerniastudent.se>
2025-01-15 12:58:33 +0100

    Added express-server and  Kino-site with modified code to work with express server

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import fs from "fs/promises";
+
+const app = express();
+
+app.get('/', async ( request, response ) => {
+    const buf = await fs.readFile("./static/index.html");
+    const html = buf.toString();
+
+    response.send(html);
+});
+
+app.use('/static', express.static('./static/'));
+app.use('/assets', express.static('./static/assets'));
+
+app.listen(3080);
