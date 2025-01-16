869f07f189a5cf551b71dbb201f380f1ef04d6e3
Deansie <dennis.andersen@lerniastudent.se>
2025-01-16 13:56:35 +0100

    Styling of single moviecard

--- static/Script/moviecard-solo.js
+++ static/Script/moviecard-solo.js
@@ -6,26 +6,28 @@ export async function showSoloMovie(movieData) {
     const returnButton = document.querySelector('.movie__wrapper__return')
 
     const movieContent = `
-      <div>
-        <h2>${movieData.title}</h2>
-        <img src="${movieData.image.url}" alt="${movieData.title}" class="movie-modal__poster">
-        <p><strong>Description:</strong> ${movieData.intro || 'No description available.'}</p>
+      <div class="moviedata__div">
+        <div><h2>${movieData.title}</h2></div>
+        <div><img src="${movieData.image.url}" alt="${movieData.title}" class="movie-modal__poster"></div>
+        <div class="movie__description"><p><strong>Description:</strong> ${movieData.intro || 'No description available.'}</p></div>
       </div>
     `
     const returnButtonContent = `
         <div>
-        <button class="movie__wrapper__return" type="button">Tillbaka</button>
+        <button class="movie__wrapper__returnbutton" type="button">Tillbaka</button>
         </div>
     `
     returnButton.innerHTML = returnButtonContent
     soloMovieContainer.innerHTML = movieContent
     soloMovieContainer.classList.add('active')
     movieContainer.classList.add('inactive')
+    returnButton.classList.add('active')
     
 
     document.querySelector(".movie__wrapper__return").addEventListener('click', function () {
       soloMovieContainer.classList.remove('active')
       movieContainer.classList.remove('inactive')
+      returnButton.classList.remove('active')
       
     })
 }
